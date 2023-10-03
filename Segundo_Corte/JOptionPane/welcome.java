b894f3017c269ed5c740ac8ccce03d01da47d6ac
Samulamux <54162038+Samulamux@users.noreply.github.com>
2023-10-03 17:03:53 -0500

    Add files via upload

---
+++ Segundo_Corte/JOptionPane/welcome.java
@@ -0,0 +1,10 @@
+package scm.JOptionPane;
+
+import javax.swing.JOptionPane;
+
+public class welcome {
+   public static void main (String[] args) {
+       JOptionPane.showMessageDialog(null, "!Welcome\nto\nthe Java World!");
+       System.exit(0);
+   }
+}
