5ec47b011c8ca8f96f25ebca01ffb6279f6b145f
Samulamux <54162038+Samulamux@users.noreply.github.com>
2023-10-18 13:42:00 -0500

    Add files via upload

---
+++ Segundo_Corte/IntroduccionHerencia/test/TestHerencia.java
@@ -0,0 +1,14 @@
+package test;
+
+import herencia.*;
+import java.util.Date;
+
+public class TestHerencia {
+    public static void main(String[] args){
+        
+        Empleado empleado1 = new Empleado("Alex",3000);
+        System.out.println(empleado1);
+        Empleado empleado2 = new Empleado("Juan",4000);
+        System.out.println(empleado2);
+    }    
+}
