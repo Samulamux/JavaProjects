edbd12f3f01c11bb16f3f4d57645ec64434493f4
Samulamux <54162038+Samulamux@users.noreply.github.com>
2023-11-01 13:35:01 -0500

    Update TestHerencia.java

--- Segundo_Corte/IntroduccionHerencia/test/TestHerencia.java
+++ Segundo_Corte/IntroduccionHerencia/test/TestHerencia.java
@@ -6,13 +6,18 @@ import java.util.Date;
 public class TestHerencia {
     public static void main(String[] args){
         
-        Empleado empleado1 = new Empleado("Alex",3000);
-        System.out.println(empleado1);
-        Empleado empleado2 = new Empleado("Juan",4000);
-        System.out.println(empleado2);
+        Person persona = new Person("Juan",'M', 25, "Cusco");
+        System.out.println(persona.obtenerDetalle());
+        
+        Empleado empleado1 = new Empleado(3000, "Alex", 23);
+        System.out.println(empleado1.obtenerDetalle());
         
         var fecha = new Date();
         Cliente cliente1 = new Cliente(fecha,true,"Suny",'F',18,"Santo Tomas");
-        System.out.println(cliente1);
-    }    
+        System.out.println(cliente1.obtenerDetalle());
+    }
+    
+    public static void imprimir(Person personaje) {
+        System.out.println(personaje.obtenerDetalle());
+    }
 }
